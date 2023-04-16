8f8d45e061b763d94793295075980a333971bcaf
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-16 12:23:13 +0200

    Update OAuthStore.ts
    In this version, I added comments explaining what each function does and added more descriptive function names. I also added a constructor that checks if the user was redirected from OAuth and calls the appropriate function.

--- website/src/stores/OAuthStore.ts
+++ website/src/stores/OAuthStore.ts
@@ -3,6 +3,7 @@ import dispatcher from '../dispatcher';
 import queryString from 'query-string';
 import Constants from '../Constants';
 
+// Initialize variables
 let shouldPlayVideo = false;
 let onMessage;
 
@@ -10,52 +11,60 @@ class OAuthStore extends EventEmitter {
   constructor() {
     super();
 
+    // Check if redirected from OAuth
     const keyToSuccess = queryString.parse(window.location.search).key_to_success;
     if (keyToSuccess == '1') {
-      this.redirectedFromOAuth(true);
+      this.redirectedFromOAuth(true); // User successfully added bot
     } else if (keyToSuccess == '0') {
-      this.redirectedFromOAuth(false);
+      this.redirectedFromOAuth(false); // User failed to add bot
     }
   }
 
+  // Start OAuth process
   startOAuth() {
     shouldPlayVideo = false;
     window.removeEventListener('message', onMessage);
-    window.open('/login', '', 'height=800, width=500');
+    window.open('/login', '', 'height=800, width=500'); // Open login page in new window
     onMessage = this.onMessage.bind(this);
-    window.addEventListener('message', onMessage);
-    this.emit('change');
+    window.addEventListener('message', onMessage); // Listen for messages from login page
+    this.emit('change'); // Emit change event
   }
 
+  // Handle message from login page
   onMessage({ data }: { data: string }) {
-    if (data == Constants.Message.OAUTH_ADDED) {
+    if (data == Constants.Message.OAUTH_ADDED) { // Bot added successfully
       this.endOAuth();
     }
   }
 
+  // End OAuth process
   endOAuth() {
     window.removeEventListener('message', onMessage);
     shouldPlayVideo = true;
-    this.emit('change');
+    this.emit('change'); // Emit change event
   }
 
+  // Redirect from OAuth process
   redirectedFromOAuth(addedBot: boolean) {
     if (addedBot && window.opener) {
-      window.opener.postMessage(Constants.Message.OAUTH_ADDED, '*');
+      window.opener.postMessage(Constants.Message.OAUTH_ADDED, '*'); // Send message to parent window
     }
 
-    window.close();
+    window.close(); // Close current window
   }
 
+  // Video played
   playedVideo() {
     shouldPlayVideo = false;
-    this.emit('change');
+    this.emit('change'); // Emit change event
   }
 
+  // Check if video should play
   shouldPlayVideo(): boolean {
     return shouldPlayVideo;
   }
 
+  // Handle actions
   handle({ type, addedBot }: { type: string; addedBot: boolean }) {
     switch (type) {
       case Constants.Event.OAUTH_START: {
@@ -80,6 +89,7 @@ class OAuthStore extends EventEmitter {
 
 const oAuthStore = new OAuthStore();
 
+// Subscribe to dispatcher
 dispatcher.subscribe(oAuthStore.handle.bind(oAuthStore));
 
 export default oAuthStore;
